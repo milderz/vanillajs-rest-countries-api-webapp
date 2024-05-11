e4a1e1956930f40736dcf2846e58fc98b9f4859d
milderz <mzunax@gmail.com>
2024-05-10 22:17:31 -0600

    remove render function

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const countriesSection = document.querySelector('.countries')
 
-const renderCountries = () => {
 
-}
 
 const fetchCountries = async (url) => {
     const res = await fetch(url)
     const data = await res.json()
 
     data.map((country) => {
+        const slug = country.name.common.toLowerCase().split(' ').join('-')
         const countryCard = document.createElement('div')
         countryCard.innerHTML = `<article>
     <img src="${country.flags.png}" alt"${country.flags.alt}" height="160" width="264">
     <h2>${country.name.common}</h2>
+    <a href="${slug}">${country.name.common}</a>
     <p><strong>Population: </strong>${country.population}</p>
     <p><strong>Region: </strong>${country.region}</p>
     <p><strong>Capital: </strong>${country.capital}</p>
