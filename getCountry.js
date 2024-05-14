d9cd195ee3c828b95496eb56fe65741c920e6958
milderz <75046268+milderz@users.noreply.github.com>
2024-05-14 03:50:57 +0000

    update contry detail page

--- getCountry.js
+++ getCountry.js
@@ -2,4 +2,8 @@ const countryName = document.querySelector('.country-name')
 
 const query = new URLSearchParams(window.location.search)
 const params = query.get('name')
-countryName.innerHTML = params
+
+
+
+
+
