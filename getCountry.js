0ecf8b204047dda56b3466225abb1bd2bcbd518f
Milder <mzunax@gmail.com>
2024-05-19 20:46:28 -0600

    set API information to country detail page

--- getCountry.js
+++ getCountry.js
@@ -1,21 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-const countryMain = document.querySelector('main')
-const query = new URLSearchParams(window.location.search)
-const params = query.get('name')
-const countrySearch = params.split('-').join(' ')
-// countryName.textContent = params.split('-').join(' ')
+    const countryMain = document.querySelector('main')
+    const query = new URLSearchParams(window.location.search)
+    const params = query.get('name')
+    const countrySearch = params.split('-').join(' ')
+    // countryName.textContent = params.split('-').join(' ')
 
-const fetchCountry = async (country) => {
-    const res = await fetch(`https://restcountries.com/v3.1/name/${country}`)
-    const data = await res.json()
-    console.log(data);
+    const fetchCountry = async (country) => {
+        const res = await fetch(`https://restcountries.com/v3.1/name/${country}`)
+        const data = await res.json()
+        console.log(data);
 
-    
-    
-    data.map((country) => {
-        const countryDetail = document.createElement('section')
-        countryDetail.innerHTML = `<section class="country-detail">
+
+
+        data.map((country) => {
+            const countryDetail = document.createElement('section')
+            let currencyCode = Object.keys(country.currencies)[0]
+            let currencyName = country.currencies[currencyCode].name
+            let languageCodes = Object.keys(country.languages) 
+            let languageNames = languageCodes.map((langCode) => country.languages[langCode])
+            console.log(languageNames);
+
+                countryDetail.innerHTML = `<section class="country-detail">
+        
         <img src="${country.flags.png}"
             alt="" height="400" width="600">
         <article>
@@ -25,16 +32,16 @@ const fetchCountry = async (country) => {
             </header>
             <div>
                 <div class="col-1">
-                    <p><strong>Native name: </strong>${country.name.nativeName.ron.official}</p>
-                    <p><strong>Population: </strong></p>
-                    <p><strong>Region: </strong></p>
-                    <p><strong>Subregion: </strong></p>
-                    <p><strong>Capital: </strong></p>
+                    <p><strong>Native name: </strong></p>
+                    <p><strong>Population: </strong>${country.population}</p>
+                    <p><strong>Region: </strong>${country.region}</p>
+                    <p><strong>Subregion: </strong>${country.subregion}</p>
+                    <p><strong>Capital: </strong>${country.capital}</p>
                 </div>
                 <div class="col-2">
-                    <p><strong>Top Level Domain: </strong></p>
-                    <p><strong>Currencies: </strong></p>
-                    <p><strong>Languages: </strong></p>
+                    <p><strong>Top Level Domain: </strong>${country.tld}</p>
+                    <p><strong>Currencies: </strong>${currencyName}</p>
+                    <p><strong>${languageNames.length > 1 ? `Languages: ` : `Language: `}</strong>${languageNames}</p>
                 </div>
             </div>
             <footer>
@@ -46,11 +53,11 @@ const fetchCountry = async (country) => {
 
 
     </section>`
-    countryMain.append(countryDetail)
-    })
-}
+            countryMain.append(countryDetail)
+        })
+    }
 
-fetchCountry(countrySearch)
+    fetchCountry(countrySearch)
 
 
 
